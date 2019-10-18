d81dea72a71e8a3f80e7f553f55b3c877835f340
Frank Gerold <franketsgerold@gmail.com>
2019-10-18 20:14:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,9 @@
 def nyc_pigeon_organizer(data)
   name_keyed_list = data.reduce({}) do
     |memo, (category, subcategories)|
-    
+    subcategories.each do
+      |(cat, names)|
+      
   end
   name_keyed_list
 end
